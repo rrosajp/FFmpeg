fc4b5235963441cf29fa3c9f0f6d6ad1e61b8749
Araz Iusubov <Primeadvice@gmail.com>
2026-07-13 16:02:35 +0200

    avfilter/vf_vqe_amf: Add AMF Video Quality Enhancer filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -444,6 +444,7 @@ extern const FFFilter ff_vf_scale;
 extern const FFFilter ff_vf_vpp_amf;
 extern const FFFilter ff_vf_sr_amf;
 extern const FFFilter ff_vf_frc_amf;
+extern const FFFilter ff_vf_vqe_amf;
 extern const FFFilter ff_vf_scale_cuda;
 extern const FFFilter ff_vf_scale_d3d11;
 extern const FFFilter ff_vf_scale_d3d12;
